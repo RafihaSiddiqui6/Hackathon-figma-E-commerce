e513859461a9656b9a85675b22bee966d28fbda1
Rafiha Siddiqui <164239861+RafihaSiddiqui6@users.noreply.github.com>
2024-12-18 11:42:12 +0500

    product.tsx

--- src/app/components/product.tsx
+++ src/app/components/product.tsx
@@ -5,12 +5,10 @@ import React from 'react'
 
 export default function Products(){
   return (
-    <>
+    <div>
       <section>
         <div className='px-8 py-12 text-[#2A254B] mt-12'>
           <h1 className='text-2xl'>Our popular products</h1>
-
-          
           <div className='flex flex-col md:flex-row gap-8 mt-8'>
             
             {/* Product 1 */}
@@ -20,7 +18,7 @@ export default function Products(){
                 height={800}
                 width={800}
                 alt='sofa'
-                className='w-full h-[80%] object-cover transition-transform duration-300 ease-in-out group-hover:scale-105'
+                className='w-full h-[80%]'
               />
               <div className='mt-4 text-[#2A254B]'>
                 <p className='py-2'>The popular suede sofa</p>
@@ -35,7 +33,7 @@ export default function Products(){
                 height={800}
                 width={800}
                 alt='chair'
-                className='w-full h-[80%] object-cover transition-transform duration-300 ease-in-out group-hover:scale-105'
+                className='w-full h-[80%]'
               />
               <div className='mt-4 text-[#2A254B]'>
                 <p className='py-2'>The Dandy chair</p>
@@ -50,7 +48,7 @@ export default function Products(){
                 height={900}
                 width={900}
                 alt='chair'
-                className='w-full h-[80%] object-cover transition-transform duration-300 ease-in-out group-hover:scale-105'
+                className='w-full h-[80%]'
               />
               <div className='mt-4 text-[#2A254B]'>
                 <p className='py-2'>The Dandy chair</p>
@@ -61,14 +59,14 @@ export default function Products(){
           </div>
 
           {/* {button} */}
-          <div className='my-10 flex justify-center items-center'>
+          <div className='mb-0 mt-4 flex justify-center items-center'>
             <a href="/productlist" className='bg-[#F9F9F9] py-4 px-6 rounded-[5px] font-semibold text-[#2A254B] hover:bg-[#6e65a8]'>
               View products
             </a>
           </div>
         </div>
       </section>
-    </>
+    </div>
   )
 }
 
