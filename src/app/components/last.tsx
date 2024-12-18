423369e47b5007db56cda38ed1b5da2d303372c6
Rafiha Siddiqui <164239861+RafihaSiddiqui6@users.noreply.github.com>
2024-12-18 11:40:57 +0500

    last.tsx

--- src/app/components/last.tsx
+++ src/app/components/last.tsx
@@ -2,22 +2,22 @@ import Image from 'next/image'
 import React from 'react'
 export default function Last(){
   return (
-    <>
+    <div>
       <section>
-        <div className="py-12 text-[#2A254B] mt-12 ">
+        <div className=" text-[#2A254B]  ">
           <div className="flex flex-col md:flex-row">
-            <div className="w-full md:w-[720px] h-auto px-4 md:px-12 py-8 flex flex-col justify-between">
+            <div className="w-full md:w-[720px] h-[603px] px-4 md:px-12 py-8 flex flex-col justify-between">
               <div>
                 <h1 className="text-2xl md:text-2xl">
                   From a studio in London to a global brand with <br /> over 400 outlets
                 </h1>
                 <h1 className="py-6 text-lg md:text-xl">
-                  When we started Avion, the idea was simple. Make high-quality furniture <br />
+                  When we started Avion, the idea was simple. Make high-quality furniture
                   affordable and available for the mass market.
                 </h1>
                 <h1 className="text-base md:text-lg">
-                  Handmade and lovingly crafted furniture and homeware is what we live, <br />
-                  breathe, and design, so our Chelsea boutique became the hotbed for the <br />
+                  Handmade and lovingly crafted furniture and homeware is what we live, 
+                  breathe, and design, so our Chelsea boutique became the hotbed for the 
                   London interior design community.
                 </h1>
               </div>
@@ -28,20 +28,19 @@ export default function Last(){
               </div>
             </div>
 
-            <div className="w-full md:w-[890px] h-auto">
+             <div className="w-full md:w-[720px] h-auto">
               <Image
                 src={'/images/blend.png'}
-                height={800}
-                width={800}
+                height={1500}
+                width={1500}
                 alt="chair"
-                className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-105 hover:rotate-2"
+                className="w-[720px] h-[603px] "
               />
-            </div>
-          </div>
+            </div> 
+        </div>
         </div>
       </section>
-    </>
+    </div>
   )
 }
-
 
