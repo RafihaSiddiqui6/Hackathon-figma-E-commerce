78fa3e2df81d9a8f5932583446e4faa3b6fb6b46
Rafiha Siddiqui <164239861+RafihaSiddiqui6@users.noreply.github.com>
2024-12-18 13:20:52 +0500

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Brand from "./components/brand"
-import Ceramics from "./components/ceramics"
-import Club from "./components/club"
-import Products from "./components/product"
 import Uniqueness from "./components/uniqueness"
+import Ceramics from "./components/ceramics"
+import Products from "./components/product"
+import Club from "./components/club"
 import Last from "./components/last"
 export default function Page(){
   return (
